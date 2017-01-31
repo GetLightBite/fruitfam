bd25beb62d888082f18a3bb61def837aa3891deb
Abhinav Vadrevu <abhinav@kalekam.com>
2017-01-30 22:53:43 -0800

    config changes

--- fruitfam/scripts/console.py
+++ fruitfam/scripts/console.py
@@ -1,3 +1,4 @@
 from fruitfam import app, auth, db
 from fruitfam.models.user import User
 from fruitfam.models.component import Component
+from fruitfam.models.food_item import FoodItem
--- run.py
+++ run.py
@@ -1,4 +1,19 @@
-# Run a test server.
+# set env variables
+import argparse
+import os
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-p', '--prod', help='deploy in production!', action='store_true')
+  parsed_args = parser.parse_args()
+  if parsed_args.prod:
+    print 'Env is PROD'
+    os.environ["ENV"] = "PROD"
+    os.environ["APP_CONFIG_FILE"] = "../config/prod.py"
+  else:
+    print 'Env is DEVEL'
+    os.environ["ENV"] = "DEVEL"
+    os.environ["APP_CONFIG_FILE"] = "../config/devel.py"
 
 from fruitfam import app
-app.run(host='0.0.0.0', port=5000, debug=True)
+app.run(host='0.0.0.0', port=5000, use_reloader=False)
