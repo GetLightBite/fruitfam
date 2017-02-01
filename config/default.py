7092849a19052c5ba139badd548384a0db63220c
Abhinav Vadrevu <abhinav@kalekam.com>
2017-01-31 16:56:31 -0800

    wip

--- config/default.py
+++ config/default.py
@@ -2,5 +2,5 @@ SECRET_KEY = 'booty call time'
 
 CELERY_BROKER_URL = "redis://h:p2a8198acce063239e9ad645c1a7c90d71ff62d0cb9e202e531931b19dfc7854d@ec2-34-198-124-158.compute-1.amazonaws.com:9269"
 CELERY_ACCEPT_CONTENT = ['pickle', 'json', 'msgpack', 'yaml']
-CELERY_TASK_SERIALIZER = ['pickle', 'json', 'msgpack', 'yaml']
-CELERY_RESULT_SERIALIZER = ['pickle', 'json', 'msgpack', 'yaml']
+CELERY_TASK_SERIALIZER = 'pickle'
+CELERY_RESULT_SERIALIZER = 'pickle'
