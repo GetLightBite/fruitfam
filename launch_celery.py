da46faae05546f4c99ba6bba9a60a39d1a8679f5
Abhinav Vadrevu <abhinav@kalekam.com>
2017-02-06 13:11:01 -0800

    updated celery workers, log level

--- launch_celery.py
+++ launch_celery.py
@@ -33,4 +33,4 @@ if __name__ == '__main__':
     print 'Env is DEVEL'
     os.environ["ENV"] = "DEVEL"
     os.environ["APP_CONFIG_FILE"] = "../config/devel.py"
-  run('celery -A fruitfam.tasks worker --loglevel=info')
+  run('celery -A fruitfam.tasks worker --loglevel=DEBUG -P gevent')
