1f5ee5ee4360819e39dfbb597e5f25c3f607b68d
Abhinav Vadrevu <abhinav@kalekam.com>
2017-02-09 14:10:17 -0800

    wip

--- fruitfam/tasks/__init__.py
+++ fruitfam/tasks/__init__.py
@@ -5,12 +5,15 @@ print 'Clearing queue'
 discard_all()
 
 # Reschedule notifs for all current missions
-from fruitfam.models.user_mission import UserMission
-current_user_missions = UserMission.query.filter(
-  is_over=False
-).all()
-for mission in current_user_missions:
-  rules = mission.get_rules()
-  rules.schedule_notifs()
+def reschedule_notifs():
+  from fruitfam.models.user_mission import UserMission
+  current_user_missions = UserMission.query.filter(
+    is_over=False
+  ).all()
+  for mission in current_user_missions:
+    rules = mission.get_rules()
+    rules.schedule_notifs()
+
+reschedule_notifs()
 
 from fruitfam import celery
