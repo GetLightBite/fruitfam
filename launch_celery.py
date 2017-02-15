893e4b2341f154d5a94205a3e6a6b09cdae8a603
Abhinav Vadrevu <abhinav@kalekam.com>
2017-02-14 22:37:54 -0800

    deploy changes

--- launch_celery.py
+++ launch_celery.py
@@ -25,13 +25,13 @@ if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument('-p', '--prod', help='run with prod db!', action='store_true')
   parsed_args = parser.parse_args()
-  if parsed_args.prod:
-    print 'Env is PROD'
-    os.environ["ENV"] = "PROD"
-    os.environ["APP_CONFIG_FILE"] = "../config/prod.py"
-  else:
-    print 'Env is DEVEL'
-    os.environ["ENV"] = "DEVEL"
-    os.environ["APP_CONFIG_FILE"] = "../config/devel.py"
+  # if parsed_args.prod:
+  #   print 'Env is PROD'
+  #   os.environ["ENV"] = "PROD"
+  #   os.environ["APP_CONFIG_FILE"] = "../config/prod.py"
+  # else:
+  #   print 'Env is DEVEL'
+  #   os.environ["ENV"] = "DEVEL"
+  #   os.environ["APP_CONFIG_FILE"] = "../config/devel.py"
   run('celery -f -A fruitfam.tasks purge')
   run('celery -A fruitfam.tasks worker --loglevel=DEBUG -P gevent')
