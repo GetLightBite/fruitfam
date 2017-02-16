7c8959675cb1c3511c6202ec45800fee456ae929
Abhinav Vadrevu <abhinav@kalekam.com>
2017-02-16 15:46:38 -0800

    notifs only get scheduled once

--- fruitfam/tasks/__init__.py
+++ fruitfam/tasks/__init__.py
@@ -5,12 +5,12 @@ from fruitfam.tasks.notifications import *
 from fruitfam.tasks.update_food_item import *
 
 from celery.task.control import discard_all
+discard_all()
 # # Clear the redis queue
 # print 'Clearing queue'
 
 # Reschedule notifs for all current missions
 def reschedule_notifs():
-  discard_all()
   from fruitfam.models.user_mission import UserMission
   current_user_missions = UserMission.query.filter_by(
     is_over=False
@@ -19,6 +19,6 @@ def reschedule_notifs():
     rules = mission.get_rules()
     rules.schedule_notifs()
 
-reschedule_notifs()
+# reschedule_notifs()
 
 from fruitfam import celery
