3c35609575c10ab40df2bcb2737c65ed2e0e65db
Abhinav Vadrevu <abhinav@kalekam.com>
2017-02-16 18:11:32 -0800

    emails from upload

--- fruitfam/views/core.py
+++ fruitfam/views/core.py
@@ -239,7 +239,7 @@ def delete_user():
   )
 
 @app.route('/delete/food_item', methods=['POST'])
-def delete_user():
+def delete_food_item():
   food_item_id = request.json['foodItemId']
   f = FoodItem.query.filter_by(id=food_item_id).one()
   db.session.delete(f)
